3c0a27862edbb9168b3a1aeec8680d4de8ddaf89
Varigarble <58060789+Varigarble@users.noreply.github.com>
2020-02-17 21:48:42 -0800

    created all_vendors_report

--- sam_records_json_reports.py
+++ sam_records_json_reports.py
@@ -2,6 +2,7 @@ import json
 import sqlite3
 from sqlite3 import Error
 
+
 def create_connection(db_file):
     connect = None
     try:
@@ -21,16 +22,29 @@ all_tables = c.fetchall()
 tables_reformatted = [table[0] for table in all_tables]
 
 
+def all_vendors_report():
+    global tables_reformatted
+    json_dict = ({"Software Vendors:": tables_reformatted})
+    sv_json = open("C:\\Users\Ghuleh\Documents\GitHub\serial-number-format-validator\sam_vendors.json", "w",
+                   encoding="utf-8")
+    json.dump(json_dict, sv_json, ensure_ascii=False, indent=4, separators=(',', ': '))
+    sv_json.close()
+
+
+all_vendors_report()
+
+
 def all_records_report():
     global tables_reformatted
     json_dict = ({
-            "Software Vendor": tables_reformatted})
-        #     "s/n": serials,
-        #     "Product Key": pk_in
-        # })
+        "Software Vendor": tables_reformatted})
+    #     , "s/n": serials,
+    #     "Product Key": pk_in
+    # })
     print(json_dict)
-    sr_json = open("C:\\Users\Ghuleh\Documents\GitHub\serial-number-format-validator\sam_records.json", "w", encoding="utf-8")
-    json.dump(json_dict, sr_json, ensure_ascii = False, indent=4, separators=(',', ': '))
+    sr_json = open("C:\\Users\Ghuleh\Documents\GitHub\serial-number-format-validator\sam_records.json", "w",
+                   encoding="utf-8")
+    json.dump(json_dict, sr_json, ensure_ascii=False, indent=4, separators=(',', ': '))
     sr_json.close()
 
 
