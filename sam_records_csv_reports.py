f2649d149f01997cb05bf3b2698ae906afb189ee
Varigarble <58060789+Varigarble@users.noreply.github.com>
2020-02-17 20:28:18 -0800

    created_all_records_report

--- sam_records_csv_reports.py
+++ sam_records_csv_reports.py
@@ -13,20 +13,40 @@ def create_connection(db_file):
     finally:
         return connect
 
+
 conn = create_connection("sam_records.db")
 c = conn.cursor()
+c.execute("SELECT NAME from sqlite_master where type=='table'")
+all_tables = c.fetchall()
+tables_reformatted = [table[0] for table in all_tables]
 
-def all_tables_report():
-    global c
-    c.execute("SELECT NAME from sqlite_master where type=='table'")
-    all_tables = c.fetchall()
-    # return all_tables
-    with open("sam_records.csv", "w", newline='') as sr_csv:
+
+def all_vendors_report():
+    global all_tables
+    with open("sam_vendors.csv", "w", newline='') as sr_csv:
         csv_writer = csv.writer(sr_csv)
         for vendor in all_tables:
             csv_writer.writerow(vendor)
 
-# for name in all_tables_report():
-#     print(name)
 
-all_tables_report()
+all_vendors_report()
+
+
+def all_records_report():
+    global tables_reformatted
+    with open("sam_records.csv", "w", newline='') as sr_csv:
+        headers = ["Software Vendor", "s/n", "Product Key"]
+        csv_writer = csv.DictWriter(sr_csv, fieldnames=headers)
+        csv_writer.writeheader()
+        i = 0
+        for vendor in tables_reformatted:
+            csv_writer.writerow({
+                "Software Vendor": tables_reformatted[i],})
+                # "s/n": serials[i],
+                # "Product Key": pk_in[i]})
+            i += 1
+
+
+all_records_report()
+
+
